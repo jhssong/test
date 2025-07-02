d3b77f11e170df8640c91105c698873e0aea8abd
jhssong <jhssong02@gmail.com>
2025-07-02 14:23:21 +0900

    test

--- .scripts/setup_githooks.sh
+++ .scripts/setup_githooks.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
-git config core.hooksPath githooks
-echo "✅ Git hook path configured"
+
+# move to the git project root no matter where the script is run
+repo_root=$(git rev-parse --show-toplevel)
+hook_path="$repo_root/githooks"
+
+git config core.hooksPath "$hook_path"
+
+echo "✅ Git hooks path set to: $hook_path"
+
+#git config core.hooksPath githooks
+#echo "✅ Git hook path configured"
 
